8776a03e5b358f92aa6280eb69bec02ab243d48c
ridho56 <83654550+ridho56@users.noreply.github.com>
2021-12-22 22:09:33 +0700

    Add files via upload

---
+++ UAS-Ridho-Program/UAS-Ridho-Program.ino
@@ -0,0 +1,66 @@
+// Present a "Will be back soon web page", as stand-in webserver.
+// 2011-01-30 <jc@wippler.nl>
+//
+// License: GPLv2
+
+#include <EtherCard.h>
+
+#define STATIC 1  // set to 1 to disable DHCP (adjust myip/gwip values below)
+
+#if STATIC
+// ethernet interface ip address
+static byte myip[] = { 192,168,43,200 };
+// gateway ip address
+static byte gwip[] = { 192,168,43,195 };
+#endif
+
+// ethernet mac address - must be unique on your network
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+byte Ethernet::buffer[500]; // tcp/ip send and receive buffer
+
+const char page[] PROGMEM =
+"HTTP/1.0 503 My Service \r\n"
+"Content-Type: text/html\r\n"
+"Retry-After: 600\r\n"
+"\r\n"
+"<html>"
+  "<head><title>"
+    "Halaman Data Diri"
+  "</title></head>"
+  "<body>"
+    "<h3><strong>Data Diri Saya</strong></h3>"
+    "<h6>Nama = Ridho Waldhayin Rifai</h6>"
+    "<h6>Nim  = V3920052</h6>"
+    "<h6>Tanggal Lahir = 5 Juli 2002</h6>"
+    "<h6>Email = ridhorifai88@student.uns.ac.id</h6>"
+  "</body>"
+"</html>"
+;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("\n[backSoon]");
+
+  // Change 'SS' to your Slave Select pin, if you arn't using the default pin
+  if (ether.begin(sizeof Ethernet::buffer, mymac, 10) == 0)
+    Serial.println( "Failed to access Ethernet controller");
+#if STATIC
+  ether.staticSetup(myip, gwip);
+#else
+  if (!ether.dhcpSetup())
+    Serial.println("DHCP failed");
+#endif
+
+  ether.printIp("IP:  ", ether.myip);
+  ether.printIp("GW:  ", ether.gwip);
+  ether.printIp("DNS: ", ether.dnsip);
+}
+
+void loop(){
+  // wait for an incoming TCP packet, but ignore its contents
+  if (ether.packetLoop(ether.packetReceive())) {
+    memcpy_P(ether.tcpOffset(), page, sizeof page);
+    ether.httpServerReply(sizeof page - 1);
+  }
+}
